fe264f10795bbed89e6521d85d03c0b12ec1d87a
BlackFlame33 <blackflame33@icloud.com>
2020-11-29 16:09:36 +0800

    feat: install script
    添加安装脚本
    
    BREAKING CHANGE: 首次添加安装脚本

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+cd ~/.vim_runtime
+
+echo 'set runtimepath+=~/.vim_runtime
+
+try
+source ~/.vim_runtime/my_configs.vim
+catch
+endtry
+
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
+' > ~/.vimrc
+
+echo "Installed BlackFlame33's vimrc successfully! Enjoy :-)"
